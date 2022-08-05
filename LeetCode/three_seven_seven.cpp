a92b3017ff8a7a442783dcd17c38548c22fd13b0
aSquaredRush <57681507+aSquaredRush@users.noreply.github.com>
2022-08-05 09:57:59 -0400

    Create three_seven_seven.cpp

---
+++ LeetCode/three_seven_seven.cpp
@@ -0,0 +1,98 @@
+
+//DISCLAIMER:
+// THIS SOLUTION DOES NOT WORK ON LEETCODE DUE TO SOME RANDOM RUNTIME ERROR
+// ,HOWEVER, BECAUSE IT WORKS ON CSES WHICH ACCEPTS LARGER TEST CASES, I'VE DECIDED TO INCLUDE IT HERE.
+
+
+
+
+#ifndef COMP_LEETCODEBITS_H
+#define COMP_LEETCODEBITS_H
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+#define FOR(i,a,b) for(int i = (a); i<(b); i++)
+#define fr(i,a) FOR(i,0,a)
+
+
+#define fast ios::sync_with_stdio(0); cin.tie(0)
+#define ll long long
+#define vl vector<ll>
+#define vi vector<int>
+#define vvi vector<vi>
+
+#define sz(a) ((int)a.size())
+#define all(a) a.begin(), a.end()
+#define pb(a) push_back(a)
+#endif //COMP_LEETCODEBITS_H
+
+class Solution {
+public:
+unsigned int combinationSum4(vector<int>& nums, int target) {
+    vector<unsigned int> dp(target+1,0);
+        fr(i,nums.size()) dp[nums[i]]++;
+        for(int i = 1;i<=target;i++){
+          fr(j,nums.size()){
+            if(i-nums[j]>=0) dp[i]+=dp[i-nums[j]];
+          }
+     }
+
+    return dp[target];
+
+
+}
+};
